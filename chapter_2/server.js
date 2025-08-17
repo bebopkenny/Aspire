638c2193934b7c18d58584d909f3ffa50d70cb98
Kenny Garcia <kennygarcia15@csu.fullerton.edu>
2025-08-16 19:01:46 -0700

    Testing

--- chapter_2/server.js
+++ chapter_2/server.js
@@ -1,6 +1,18 @@
+// The address of this server connected to the network is 
+// URL -> http://localhost:8383
+// IP -> 127.0.0.1:8383
 const express = require('express')
 const app = express()
 const PORT = 8383
 
+// HTTP VERBS AND ROUTES (or paths)
+// The method informs the nature of request and the route is a futher subdirectory
+// (basically we direct the request to the body of code to respond appropriately, and 
+// these locations or routes are called endpoints)
+app.get('/', (req, res) => {
+    // this is endpoint number 1 - /
+    console.log('Yay I hit an endpoint', req.method)
+    res.sendStatus(201)
+})
 
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`))
